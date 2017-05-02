82ae464a0dc2cc620463d3be3a4b3f204e80a1d2
rgao <rgao@rgao-virtual-machine.(none)>
2017-05-01 20:11:56 -0400

    add two modulars into the application

--- tinyos2/apps/MyApp/TestFtsp/Ftsp/FtspDataLogger.py
+++ tinyos2/apps/MyApp/TestFtsp/Ftsp/FtspDataLogger.py
@@ -8,12 +8,13 @@ AM_TEST_FTSP_MSG = 137
 class FtspMsg(tos.Packet):
     def __init__(self, packet = None):
         tos.Packet.__init__(self,
-                            [('src_addr',            'int', 2),
-                             ('counter',             'int', 2),
+                            [('src_addr',            'int', 2),  # node id
+                             ('counter',             'int', 2),  # ref number
                              ('local_rx_timestamp',  'int', 4),
-                             ('global_rx_timestamp', 'int', 4),
+                             ('global_rx_timestamp', 'int', 4),  # local time of each node
                              ('skew_times_1000000',  'int', 4),
-                             ('is_synced',           'int', 1),
+                             ('is_synced',           'int', 1),  # only node #2's state needs attention, node #1's state is always 0.
+                                                                 # z1 returns: state 0 - synchronized; state 1 - not synchronized
                              ('ftsp_root_addr',      'int', 2),
                              ('ftsp_seq',            'int', 1),
                              ('ftsp_table_entries',  'int', 2)],
